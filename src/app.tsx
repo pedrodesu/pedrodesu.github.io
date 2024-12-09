446a6428cd5cf7dd5f3da290e05eb6bf04b30651
Pedro Ferreira <pedro.ferreira@01talent.com>
2024-12-09 21:55:53 +0000

    removed routing, unnecessary

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,39 @@
-import { Router } from '@solidjs/router'
-import { FileRoutes } from '@solidjs/start/router'
-import { type Component, Suspense } from 'solid-js'
+import { onCleanup, onMount, type Component } from 'solid-js'
+import { clientOnly } from '@solidjs/start'
+import About from '~/components/sections/About'
+import Footer from '~/components/sections/Footer'
+import Hero from '~/components/sections/Hero'
+import Timeline from '~/components/sections/Timeline'
+import Scroller from '~/components/Scroller'
+import Menu from '~/components/Menu'
 import '~/app.css'
 
-const App: Component = () => (
-  <Router
-    root={props => (
-      <Suspense>{props.children}</Suspense>
-    )}
-  >
-    <FileRoutes />
-  </Router>
-)
+const SectionProvider = clientOnly(async () => ({ default: (await import('~/context/section')).SectionProvider }))
+
+const App: Component = () => {
+  onMount(() => {
+    function mouseEventPredicate(ev: MouseEvent): void {
+      document.body.style.background = `radial-gradient(300px at ${ev.clientX.toString()}px ${ev.clientY.toString()}px, rgb(var(--color-dark)), rgb(var(--color-darker)) 100%)`
+    }
+
+    document.body.addEventListener('mousemove', mouseEventPredicate)
+    onCleanup(() => {
+      document.body.removeEventListener('mousemove', mouseEventPredicate)
+    })
+  })
+
+  return (
+    <SectionProvider>
+      <Menu />
+      <Scroller />
+      <main class="*:snap-start *:h-screen">
+        <Hero />
+        <About />
+        <Timeline />
+        <Footer />
+      </main>
+    </SectionProvider>
+  )
+}
 
 export default App
